311eccb380a29644ec775893fa23bc8fb4c4f856
marcio.soares <marcio.soares@teckavan.com>
2021-11-04 09:48:41 -0300

    finalizando

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {storeToRefs} from "pinia";
+import {useCounterStore} from "./store/useCounter";
+
+const main = useCounterStore();
+
+const {counter, name} = storeToRefs(main);
+
+</script>
+
+<template>
+  <h3>Hellow World</h3>
+  <h4> {{ counter }}</h4>
+  <button @click="main.addOne">Click me</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
